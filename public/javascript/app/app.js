356d97863dc134e7402f41a941fc8e27ef646c75
Kamil Augustynowicz <kamil.augustynowicz@gmail.com>
2014-07-16 11:16:49 +0200

    adding bootstrap and rating

--- public/javascript/app/app.js
+++ public/javascript/app/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var movie_app = angular.module('movieApp', []);
+var movie_app = angular.module('movieApp', ['ui.bootstrap']);
 
 movie_app.controller('MovieCtrl', [
              '$scope', 
     function ($scope) {
         this.movie = {
             title: "Back to the future",
-            director: "Robert Zemeckis"
+            director: "Robert Zemeckis",
+            rate: 8
         }
     }]
 );
