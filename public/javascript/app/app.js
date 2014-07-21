97e7dcd00566fef6fc46bfaed715e4bae7bd007e
Lukasz Sajkowski <lukasz@briisk.co>
2014-07-21 11:19:10 +0200

    movie example

--- public/javascript/app/app.js
+++ public/javascript/app/app.js
@@ -6,9 +6,19 @@ movie_app.controller('MovieCtrl', [
              '$scope', 
     function ($scope) {
         this.movie = {
-            title: "Back to the future",
-            director: "Robert Zemeckis",
-            rate: 8
-        }
+          id: 1,
+          title: "Powrót do przyszłości",
+          director: "Robert Zemeckis",
+          description: "W 1985 roku dr Emmett Brown buduje wehikuł czasu. Jego przyjaciel Marty McFly przenosi się w lata 50. i niechcący przeszkadza w poznaniu się swoim rodzicom.",
+          time: 116,
+          release: 1985,
+          image: "http://1.fwcdn.pl/po/88/23/8823/7334729.6.jpg",
+          average_rate: 7.3,
+          comments: []
+        };
+        this.user = {
+          name: "Tomek",
+          rate: 0
+        };
     }]
 );
